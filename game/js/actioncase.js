f7c1e1f251f2e6ef9b7c3fab95329b2864c419ea
mpaloulack <mpaloulack@gmail.com>
2014-09-23 12:16:34 +0200

    add function jquery

--- game/js/actioncase.js
+++ game/js/actioncase.js
@@ -1,9 +1,10 @@
-
-$.fn.getClass = function (){
-	var caseTab = document.getElementByClassName('case');
-	for (var i = 0; i < caseTab.length; i++) {
-	    if (caseTab[i].className == 'case') {
-	        console.log(caseTab);
-	    };
-	}
-};
+(function($) {
+    $.fn.getClassCase = function() {
+        var caseTab = document.getElementByClassName('case');
+        for (var i = 0; i < caseTab.length; i++) {
+            if (caseTab[i].className == 'case') {
+                console.log(caseTab);
+            };
+        }
+    };
+})(jQuery);
