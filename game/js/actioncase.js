bebe16dd3a00539932d7e73fb8ba18e34f662bd6
mpaloulack <mpaloulack@gmail.com>
2014-09-23 12:03:20 +0200

    ActionCase
    ajout du tableau de case

--- game/js/actioncase.js
+++ game/js/actioncase.js
@@ -1,8 +1,11 @@
-$.fn.getClass = funtion (){
-	var caseTab = document.getElementByClassName('case');
-	for (var i = 0; i < caseTab.length; i++) {
-	    if (caseTab[i].className == 'case') {
-	        console.log(caseTab);
-	    };
-	}
-};
+(function($) {
+    $.fn.getClassCase = function() {
+        var caseTab = document.getElementByClassName('case');
+        for (var i = 0; i < caseTab.length; i++) {
+            if (caseTab[i].className == 'case') {
+                console.log(caseTab);
+            };
+        }
+    };
+
+})(jQuery);
