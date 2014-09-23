49bfffd311e79b72b31703f55527756808fe5a49
mpaloulack <mpaloulack@gmail.com>
2014-09-23 11:40:01 +0200

    add jquery & js

---
+++ game/js/actioncase.js
@@ -0,0 +1,8 @@
+$.fn.getClass = funtion (){
+	var caseTab = document.getElementByClassName('case');
+	for (var i = 0; i < caseTab.length; i++) {
+	    if (caseTab[i].className == 'case') {
+	        console.log(caseTab);
+	    };
+	}
+};
